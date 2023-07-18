63deb49c6ac63a85fc83cbb6494a3d8cadafe24c
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-07-18 16:21:53 +0200

    minor modification

--- experiment.py
+++ experiment.py
@@ -11,7 +11,7 @@ class Experiment:
         # Setup model
         self.model = VGG16()
         temp = torchvision.models.vgg16(pretrained=True)
-        self.model.load_state_dict(temp.state_dict())
+        self.model.load_state_dict(temp.state_dict(), strict=False)
         self.model.train()
         self.model.to(self.device)
         for param in self.model.parameters():
