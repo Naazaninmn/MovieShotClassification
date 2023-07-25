6f0a562784816986cdf85c03eefd00433673af1e
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-07-25 10:30:05 +0200

    minor modification

--- experiment.py
+++ experiment.py
@@ -48,7 +48,6 @@ class Experiment:
         x = x.to(self.device)
         y = y.to(self.device)
 
-        print(data)
         logits = self.model(x)
         print(logits)
         loss = self.criterion(logits, y)
