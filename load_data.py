7093623c851e3b5547a4bdf37719c27f5a2c62e6
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-07-25 10:13:20 +0200

    minor modification

--- load_data.py
+++ load_data.py
@@ -97,7 +97,7 @@ def build_splits():
         normalize
     ])
 
-    print(test_examples)
+    print(val_examples)
     # Dataloaders
     train_loader = DataLoader(ShotDataset(train_examples, train_transform), shuffle=True)
     val_loader = DataLoader(ShotDataset(val_examples, eval_transform), shuffle=False)
