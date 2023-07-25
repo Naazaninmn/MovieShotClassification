96fea0ef5d39539430a6d79f2b912fcb74711f20
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-07-25 10:42:34 +0200

    vgg16 added

--- experiment.py
+++ experiment.py
@@ -11,9 +11,9 @@ class Experiment:
         self.device = torch.device('cpu' if opt['cpu'] else 'cuda:0')
 
         # Setup model
-        #self.model = vgg16_bn(pretrained=True)
-        #self.model.classifier[-1] = nn.Linear(in_features=4096, out_features=5)
-        self.model = MovieShotModel()
+        self.model = vgg16_bn(pretrained=True)
+        self.model.classifier[-1] = nn.Linear(in_features=4096, out_features=5)
+        #self.model = MovieShotModel()
         self.model.train()
         self.model.to(self.device)
         for param in self.model.parameters():
