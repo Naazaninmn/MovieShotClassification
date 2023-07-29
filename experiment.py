518cc65806160d41dacc5a6dad42c1b3cc68007a
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-07-29 14:22:23 +0200

    minor modification

--- experiment.py
+++ experiment.py
@@ -44,9 +44,9 @@ class Experiment:
 
         # Setup optimization procedure
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=opt['lr'])
-        self.criterion = torch.nn.CrossEntropyLoss()
+        #self.criterion = torch.nn.CrossEntropyLoss()
         #self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-5, weight_decay=1e-6)
-        #self.criterion = SupervisedContrastiveLoss(temperature=0.1).to(self.device)
+        self.criterion = SupervisedContrastiveLoss(temperature=0.1).to(self.device)
         #self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=10, eta_min=1e-6)
 
 
